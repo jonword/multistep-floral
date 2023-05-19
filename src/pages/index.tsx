153e0eb5ef9de4cd36974c809fa938a2ad7cb4b8
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-19 11:09:28 -0500

    delete forms and stepper,

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Form from "@/components/form";
-import Stepper from "@/components/stepper";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,9 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-full mt-24 flex-col items-center justify-center mx-2 mb-16">
-        <div className="flex flex-col w-full max-w-[600px] bg-gray-100 p-4 shadow-md gap-10 shadow-pink-200">
-          <Form />
-        </div>
+        <div className="flex flex-col w-full max-w-[600px] bg-gray-100 p-4 shadow-md gap-10 shadow-pink-200"></div>
       </main>
     </>
   );
