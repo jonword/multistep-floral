8818bd5a9a87458b21a679627b81e932aca97dfc
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-06-12 15:20:09 -0500

    temporary hero section, delete border on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
 import Head from "next/head";
+import Image from "next/image";
+import floral from "../assets/images/florals/IMG_9834.png";
+import { Quattrocento } from "next/font/google";
+
+const font = Quattrocento({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Home() {
   return (
@@ -9,8 +17,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-[500px] mt-24 flex justify-center w-full">
-        <div className="flex flex-col h-full w-full max-w-[700px] px-8 shadow-md shadow-pink-200"></div>
+      <main className="h-full w-full p-16">
+        <div className="flex mx-auto flex-col md:flex-row h-full w-full items-center justify-center p-4 gap-6">
+          <div>
+            <Image
+              src={floral}
+              height={400}
+              width={400}
+              alt=""
+              className=" rounded-2xl"
+            />
+          </div>
+          <div className="md:max-w-[500px]">
+            <p className={`${font.className} text-gray-200 text-lg`}>
+              My name is Renee and I am a floral designer! From arrangements,
+              bouquets, to centerpieces, I am passionate about bringing your
+              vision to life! I specialize in:
+            </p>
+            <ul
+              className={`${font.className} text-gray-200 text-lg list-disc pl-8`}
+            >
+              <li>Weddings</li>
+              <li>Events</li>
+              <li>Special Ocassions</li>
+              <li>Personal Arrangements</li>
+            </ul>
+          </div>
+        </div>
       </main>
     </>
   );
