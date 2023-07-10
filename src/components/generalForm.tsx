a07389f90fbe8e72c941f50845347bf1bc795b62
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-07-10 15:32:07 -0500

    create wedding inquiry and general inquiry

---
+++ src/components/generalForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeneralForm = () => {
+  return <div>generalForm</div>;
+};
+
+export default GeneralForm;
---
+++ src/components/weddingForm.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const WeddingForm = () => {
+  return (
+    <form
+      name="Contact"
+      method="POST"
+      data-netlify="true"
+      className="flex gap-2 w-full max-w-[600px] flex-col"
+    >
+      <label>Name:</label>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        className="p-2 border border-black rounded-md"
+      />
+      <label>Email:</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email"
+        className="p-2 border border-black rounded-md"
+      />
+      <label>Wedding Date:</label>
+      <input
+        type="text"
+        name="wedding date"
+        placeholder="mm/dd/yyyy"
+        className="p-2 border border-black rounded-md"
+      />
+      <label>Estimated Floral Budget:</label>
+      <select name="budget" className="p-2 border border-black rounded-md">
+        <option></option>
+        <option></option>
+        <option></option>
+        <option></option>
+      </select>
+      <label>Venue:</label>
+      <input
+        type="text"
+        name="venue"
+        className="p-2 border border-black rounded-md"
+      />
+      <label>Estimated Guest Count:</label>
+      <input
+        type="text"
+        name="guest count"
+        className="p-2 border border-black rounded-md"
+      />
+      <label>How many bridesmaids/groomsmen do you have?</label>
+      <input
+        type="text"
+        name="bridesmaids and groomsmen"
+        className="p-2 border border-black rounded-md"
+      />
+      <label>
+        Brief Description of Ideas (alter piece, table centerpieces, etc.):
+      </label>
+      <textarea
+        name="description"
+        className="p-2 border border-black rounded-md"
+        rows={10}
+      />
+      <label>Pinterest Link (if applicable):</label>
+      <input
+        type="text"
+        name="pinterest"
+        placeholder="https://"
+        className="p-2 border border-black rounded-md"
+      />
+      <button
+        type="submit"
+        className=" mx-auto mt-2 flex items-center border-2 border-email bg-gray-800 px-4 py-3 text-white hover:bg-green-700 rounded-md"
+      >
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default WeddingForm;
