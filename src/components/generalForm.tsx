8e2d4024fa337f0aa6d76381ead270f8ef79b4f5
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-07-11 11:10:50 -0500

    textarea isnt sending data on general form

--- src/components/generalForm.tsx
+++ src/components/generalForm.tsx
@@ -1,38 +1,56 @@
 import React from "react";
-import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ZodType, z } from "zod";
 
-const generalSchema = z
-  .object({
-    name: z.string(),
-    email: z.string().email(),
-    message: z.string(),
-  })
-  .required();
+type FormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const generalSchema: ZodType<FormData> = z.object({
+  name: z.string({ required_error: "Name is Required" }),
+  email: z.string().email(),
+  message: z.string({ required_error: "Message is Required" }),
+});
 
 const GeneralForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(generalSchema) });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <form
       name="General Inquiry"
+      onSubmit={onSubmit}
       method="POST"
       className="flex gap-2 w-full max-w-[600px] flex-col"
     >
       <label>Name:</label>
       <input
+        {...register("name")}
         type="text"
-        placeholder="Name"
         name="name"
         className="p-2 border border-black rounded-sm"
       />
       <label>Email:</label>
+
       <input
+        {...register("email")}
         type="email"
         name="email"
-        id="email"
-        placeholder="Email"
         className="p-2 border border-black rounded-sm"
       />
-      <label>Message:</label>
+      <label htmlFor="message">Message:</label>
+
       <textarea
+        {...register("message")}
+        id="message"
         name="description"
         className="p-2 border border-black rounded-sm"
         rows={10}
