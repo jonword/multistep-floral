9597424ac95fb5fa4ef610808268841432f28d1c
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-07-21 13:35:00 -0500

    get rid of inquiry forms and link buttons to google forms

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Quattrocento, Barlow } from "next/font/google";
 import WeddingForm from "@/components/weddingForm";
 import GeneralForm from "@/components/generalForm";
+import Link from "next/link";
 
 const quattrocento = Quattrocento({
   subsets: ["latin"],
@@ -14,10 +15,6 @@ const barlow = Barlow({
 });
 
 const Contact = () => {
-  const [form, setForm] = useState("general");
-
-  const handleForm = (formName: string) => setForm(formName);
-
   return (
     <>
       <h1 className={`${quattrocento.className} pb-2 text-center text-4xl`}>
@@ -27,20 +24,20 @@ const Contact = () => {
         className={`${barlow.className} flex min-h-screen w-full flex-col items-center p-8 mb-32`}
       >
         <div className="flex gap-8 mb-8">
-          <button
-            className="hover:bg-emerald-800 duration-200 bg-teal-900 p-4 text-gray-100"
-            onClick={() => handleForm("general")}
-          >
-            <p>General Inquiry</p>
-          </button>
-          <button
-            className="hover:bg-emerald-800 duration-200 bg-teal-900 p-4 text-gray-100"
-            onClick={() => handleForm("wedding")}
-          >
+          <Link href="https://forms.gle/seRXtmEnuEpjxuzd8" target="_blank">
+            <button className="hover:bg-emerald-800 duration-200 bg-teal-900 p-4 text-gray-100">
+              <p>General Inquiry</p>
+            </button>
+          </Link>
+          <Link
+            href="https://forms.gle/9wB3Chh2BpGFujug6"
+            target="_blank"
+          ></Link>
+
+          <button className="hover:bg-emerald-800 duration-200 bg-teal-900 p-4 text-gray-100">
             <p>Wedding Inquiry</p>
           </button>
         </div>
-        {form === "wedding" ? <WeddingForm /> : <GeneralForm />}
       </div>
     </>
   );
