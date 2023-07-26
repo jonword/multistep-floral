87a7607bd41666f85f4c82543a4a76c89035b319
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-07-26 11:22:03 -0500

    move home page to about page, create skeleton for photo carousel

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Image from "next/image";
+import floral from "../../../public/assets/images/about/IMG_9834.png";
+import { Barlow } from "next/font/google";
+
+const font = Barlow({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="flex mx-auto flex-col md:flex-row h-full w-full items-center justify-center p-4 gap-6">
+        <div>
+          <Image
+            src={floral}
+            height={400}
+            width={400}
+            alt=""
+            className=" rounded-lg"
+          />
+        </div>
+        <div className="md:max-w-[500px]">
+          <p className={`${font.className} text-lg`}>
+            My name is Renee and I am a floral designer! From arrangements,
+            bouquets, to centerpieces, I am passionate about bringing your
+            vision to life! I specialize in:
+          </p>
+          <ul className={`${font.className} text-lg list-disc pl-8`}>
+            <li>Weddings</li>
+            <li>Events</li>
+            <li>Special Ocassions</li>
+            <li>Personal Arrangements</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import floral from "../assets/images/florals/IMG_9834.png";
 import { Barlow } from "next/font/google";
 
 const font = Barlow({
@@ -19,28 +18,7 @@ export default function Home() {
       </Head>
       <main className="h-full w-full p-16">
         <div className="flex mx-auto flex-col md:flex-row h-full w-full items-center justify-center p-4 gap-6">
-          <div>
-            <Image
-              src={floral}
-              height={400}
-              width={400}
-              alt=""
-              className=" rounded-lg"
-            />
-          </div>
-          <div className="md:max-w-[500px]">
-            <p className={`${font.className} text-lg`}>
-              My name is Renee and I am a floral designer! From arrangements,
-              bouquets, to centerpieces, I am passionate about bringing your
-              vision to life! I specialize in:
-            </p>
-            <ul className={`${font.className} text-lg list-disc pl-8`}>
-              <li>Weddings</li>
-              <li>Events</li>
-              <li>Special Ocassions</li>
-              <li>Personal Arrangements</li>
-            </ul>
-          </div>
+          <div className="h-[400px] w-[400px] bg-gray-300 rounded-md" />
         </div>
       </main>
     </>
