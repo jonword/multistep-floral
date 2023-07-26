65091bdd2ea44d6fabc979751f4554d090509c02
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-07-26 15:38:31 -0500

    add tsx to photo component, add the props and types

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import type { Photos } from "@prisma/client";
 
-const Photo = () => {
-  return <div>Photo</div>;
+interface Props {
+  photos: Photos;
+}
+
+const PhotoCard = ({ photos }: Props) => {
+  return (
+    <>
+      <div>
+        <Image src={photos.image} alt={photos.alt} height={300} width={300} />
+      </div>
+    </>
+  );
 };
 
-export default Photo;
+export default PhotoCard;
