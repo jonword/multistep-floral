acc23f4794d345c008a1da0da79b8d2089b076f4
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-08-22 08:51:54 -0500

    general layout of homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,30 +18,21 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="flex mx-auto flex-col md:flex-row h-screen w-full items-center justify-center p-4 gap-6">
-          <div>
-            <Image
-              src={floral}
-              height={300}
-              width={300}
-              alt=""
-              className=" rounded-lg"
-            />
-          </div>
-          <div className="md:max-w-[500px]">
-            <p className={`${font.className} text-lg`}>
-              Lilium Flowers is a wedding and event floral company based in
-              Opelika, AL specializing in whimsical, modern, & detailed design.
-              The team is headed up by Floral Designer Renee Word, who is
-              devoted to working with our clients to curate a visual story and
-              whimsical experience that is unique to each couple, and reflects
-              both their lifestyle and love story. We are passionate about
-              intentionality and strive to listen to your vision and make every
-              detail of your day beautiful and unique, while giving you a
-              seamless and stress free experience.
-            </p>
-          </div>
+      <main className="flex h-screen w-full flex-col pt-24 gap-12">
+        <div className="flex justify-center md:justify-start border md:px-10">
+          <h1 className="text-4xl">Lilium Florals Animation</h1>
+        </div>
+        <div className=" flex justify-center items-center p-4 ">
+          <Image
+            src=""
+            height={400}
+            width={600}
+            alt=""
+            className="bg-gray-100 rounded-lg"
+          />
+        </div>
+        <div className="flex justify-center md:justify-end md:pr-10">
+          <h2 className="text-xl">Hero description here</h2>
         </div>
       </main>
     </>
