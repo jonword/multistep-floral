60f9fe955a0080b0263bad141a488ccc790d170e
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-11-27 11:34:49 -0600

    add rounded border to inputs and submit button

--- src/components/weddingForm.tsx
+++ src/components/weddingForm.tsx
@@ -11,14 +11,14 @@ const WeddingForm = () => {
         <input type="hidden" name="form-name" value="Contact" />
 
         <p className="">Name:</p>
-        <input type="text" name="name" className="p-2" />
+        <input type="text" name="name" className="rounded-md p-2" />
         <p className="">Email:</p>
         <input
           type="email"
           name="email"
           id="email"
           placeholder="abc@email.com"
-          className=" p-2"
+          className="rounded-md p-2"
         />
         <p className="">Phone:</p>
         <input
@@ -26,7 +26,7 @@ const WeddingForm = () => {
           name="phone"
           id="phone"
           placeholder="Phone"
-          className="p-2"
+          className="rounded-md p-2"
         />
         <p className="">Event Date:</p>
         <input
@@ -34,10 +34,10 @@ const WeddingForm = () => {
           name="date"
           id="date"
           placeholder="mm/dd/yyyy"
-          className=" p-2"
+          className="rounded-md p-2"
         />
         <p className="">Estimated Floral Budget:</p>
-        <select className="p-2">
+        <select className="rounded-md p-2">
           <option>Select One</option>
           <option>$750 - $1,200</option>
           <option>$1,200 - $3,000</option>
@@ -48,21 +48,36 @@ const WeddingForm = () => {
           <option>$15,000+</option>
         </select>
         <p>Venue:</p>
-        <input type="text" name="venue" id="venue" className=" p-2" />
+        <input type="text" name="venue" id="venue" className="rounded-md p-2" />
         <p>Estimated Guest Count:</p>
         <input
           type="text"
           name="guest count"
           id="guest count"
-          className=" p-2"
+          className="rounded-md p-2"
         />
         <p>Number of Groomsmen/Bridesmaids:</p>
-        <input type="text" name="bridal" id="bridal" className=" p-2" />
+        <input
+          type="text"
+          name="bridal"
+          id="bridal"
+          className="rounded-md p-2"
+        />
         <p>Brief Description of Ideas:</p>
-        <textarea name="message" placeholder="..." className="p-2" rows={7} />
+        <textarea
+          name="message"
+          placeholder="..."
+          className="rounded-md p-2"
+          rows={7}
+        />
         <p>Link to Pinterest Board:</p>
-        <input type="text" name="pinterest" id="pinterest" className="p-2" />
-        <button className="mx-auto mt-2 flex items-center px-12 py-4 text-lg font-bold text-white bg-gray-800 hover:bg-green-800 duration-200">
+        <input
+          type="text"
+          name="pinterest"
+          id="pinterest"
+          className="rounded-md p-2"
+        />
+        <button className="mx-auto mt-2 flex items-center px-12 py-4 text-lg font-bold text-white bg-gray-800 hover:bg-green-800 duration-200 rounded-lg">
           Send
         </button>
       </form>
